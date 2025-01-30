64be55140ae091ac1a95be0864764b7de9aa199a
Raphael <89871091+raphmn@users.noreply.github.com>
2025-01-30 14:14:51 +0100

    Delete app-gui directory

--- app-gui/auth.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <unordered_map>
-#include <string>
-
-bool AuthenticateUser(const std::string& username, const std::string& password);
--- app-gui/scan_panel.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <wx/wx.h>
-
-class ScanPanel : public wxPanel {
-public:
-    ScanPanel(wxWindow* parent);
-    void StartCameraScan();
-};
